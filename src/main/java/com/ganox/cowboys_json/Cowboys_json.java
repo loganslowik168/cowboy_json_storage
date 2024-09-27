dfaeec4248a419f3cb9e30555d3d32b0f7c3c427
loganslowik168 <loganslowik168@gmail.com>
2024-09-26 20:37:29 -0500

    Initial commit

---
+++ src/main/java/com/ganox/cowboys_json/Cowboys_json.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ganox.cowboys_json;
+
+/**
+ *
+ * @author heyin
+ */
+public class Cowboys_json {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
